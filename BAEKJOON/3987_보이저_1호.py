40ad8a449ec8e505eede1d18e226e469dac67395
송상빈 <ssbin407@naver.com>
2022-11-20 23:47:30 +0900

    221120 baekjoon 3987_보이저_1호.py

--- BAEKJOON/3987_보이저_1호.py
+++ BAEKJOON/3987_보이저_1호.py
@@ -1,66 +1,52 @@
-import sys
-sys.stdin = open('3987_input.txt', 'r')
-from collections import deque
-
-
 di = ['U', 'R', 'D', 'L']
 dy = [-1, 0, 1, 0]
 dx = [0, 1, 0, -1]
 
-T = int(input())
+N, M = map(int, input().split())
+space = [input() for _ in range(N)]
+PR, PC = map(int, input().split())
+PR -= 1
+PC -= 1
 
-for test_case in range(1, T+1):
-    N, M = map(int, input().split())
-    space = [input() for _ in range(N)]
-    PR, PC = map(int, input().split())
-    PR -= 1
-    PC -= 1
+max_signal = 0
+max_d = ''
 
-    max_signal = 0
-    max_d = ''
-    for i in range(4):
-        # print(di[i])
-        signal = 0
-        Q = deque()
-        Q.append([PR, PC, i])
-        while Q:
-            y, x, d = Q.popleft()
-            yy = y + dy[d]
-            xx = x + dx[d]
-            signal += 1
+for i in range(4):
 
-            if 0 > yy or yy == N or 0 > xx or xx == M:
-                if signal > max_signal:
-                    max_signal = signal
-                    max_d = di[i]
-                break
+    signal = 0
+    y = PR
+    x = PC
+    d = i
 
-            if space[yy][xx] == '/':
-                if d % 2:
-                    dd = d - 1
-                else:
-                    dd = d + 1
-            elif space[yy][xx] == '\\':
-                if d % 2:
-                    dd = (d + 1) % 4
-                else:
-                    dd = 3 - d
-            elif space[yy][xx] == 'C':
-                break
-            else:
-                dd = d
+    while True:
+        y = y + dy[d]
+        x = x + dx[d]
+        signal += 1
 
-            # print(yy, xx, dd, space[yy][xx])
-
-            if [yy, xx, dd] == [PR, PC, i]:
-                max_signal = 'Voyager'
+        if 0 > y or y >= N or 0 > x or x >= M or space[y][x] == 'C':
+            if signal > max_signal:
+                max_signal = signal
                 max_d = di[i]
-                break
-            else:
-                Q.append([yy, xx, dd])
-
-        if max_signal == 'Voyager':
             break
 
-    print(max_d)
-    print(max_signal)
+        if space[y][x] == '/':
+            if d % 2:
+                d = d - 1
+            else:
+                d = d + 1
+        elif space[y][x] == '\\':
+            if d % 2:
+                d = (d + 1) % 4
+            else:
+                d = 3 - d
+
+        if [y, x, d] == [PR, PC, i]:
+            max_signal = 'Voyager'
+            max_d = di[i]
+            break
+
+    if max_signal == 'Voyager':
+        break
+
+print(max_d)
+print(max_signal)
