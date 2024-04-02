182a9e57560ed78e4f056a1d5d5ab9d932526a0a
bangtugu <ssbin407@naver.com>
2024-04-02 18:06:38 +0900

    fix 카운트_다운.py

--- PROGRAMMERS/카운트_다운.py
+++ PROGRAMMERS/카운트_다운.py
@@ -56,6 +56,43 @@ def solution(target):
     return dp[target]
 
 
+def solution(target):
+    
+    single = set()
+    double = set()
+    
+    for i in range(1, 21):
+        single.add(i)
+        double.add(i*2)
+        double.add(i*3)
+    
+    single.add(50)
+    
+    dp = [[target, 0] for _ in range(target+1)]
+    for i in range(1, target+1):
+        if i in single:
+            dp[i] = [1, 1]
+        elif i in double:
+            dp[i] = [1, 0]
+        else:
+            for j in range(i-1, max(0, i-61), -1):
+                
+                if j in single:
+                    now = [dp[i-j][0]+1, dp[i-j][1]+1]    
+                elif j in double:
+                    now = [dp[i-j][0]+1, dp[i-j][1]]
+                else:
+                    now = [dp[i-j][0] + dp[j][0], dp[i-j][1] + dp[j][1]]
+                
+                if now[0] < dp[i][0]:
+                    dp[i] = now
+                elif now[0] == dp[i][0]:
+                    dp[i][1] = max(dp[i][1], now[1])
+                
+    answer = dp[target]
+    return answer
+
+
 for t in range(TC):
     answer = solution(target[t])
     correct = True if answer == result[t] else False
