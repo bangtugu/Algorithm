809ed33c4fdd83d03e4bf909c603c47fc1a5fd31
bangtugu <ssbin407@naver.com>
2024-05-21 21:03:56 +0900

    fix 퍼즐_조각_채우기.py

--- PROGRAMMERS/퍼즐_조각_채우기.py
+++ PROGRAMMERS/퍼즐_조각_채우기.py
@@ -29,80 +29,112 @@ def solution(game_board, table):
 
     dy = [0, 0, 1, -1]
     dx = [1, -1, 0, 0]
-    blank_cnt = 0
-    block_cnt = 0
-    blank_lst = []
-    block_lst = []
-    for i in range(N):
-        for j in range(N):
-            if not game_board[i][j]:
-                blank_cnt += 1
-                game_board[i][j] = blank_cnt
-                lst = [[i, j]]
-                idx = 0
-                while idx < len(lst):
-                    y, x = lst[idx]
-                    for d in range(4):
-                        ny = y + dy[d]
-                        nx = x + dx[d]
 
-                        if nx < 0 or nx >= N or ny < 0 or ny >= N or game_board[ny][nx]: continue
-                        game_board[ny][nx] = blank_cnt
-                        lst.append([ny, nx])
 
-                    idx += 1
-                blank_lst.append(lst)
+    def get_cntlst(inputlst):
+        cnt, lst = 0, []
 
-            if not table[i][j]:
-                block_cnt += 1
-                table[i][j] = block_cnt
-                lst = [[i, j]]
-                idx = 0
-                while idx < len(lst):
-                    y, x = lst[idx]
-                    for d in range(4):
-                        ny = y + dy[d]
-                        nx = x + dx[d]
+        for i in range(N):
+            for j in range(N):
+                if not inputlst[i][j]:
+                    cnt += 1
+                    inputlst[i][j] = cnt
+                    temp = [[i, j]]
+                    idx = 0
+                    while idx < len(temp):
+                        y, x = temp[idx]
+                        for d in range(4):
+                            ny = y + dy[d]
+                            nx = x + dx[d]
 
-                        if nx < 0 or nx >= N or ny < 0 or ny >= N or table[ny][nx]: continue
-                        table[ny][nx] = block_cnt
-                        lst.append([ny, nx])
-                    
-                    idx += 1
-                block_lst.append(lst)
+                            if nx < 0 or nx >= N or ny < 0 or ny >= N or inputlst[ny][nx]: continue
+                            inputlst[ny][nx] = cnt
+                            temp.append([ny, nx])
 
+                        idx += 1
+                    lst.append(temp)
+
+        return cnt, lst
     
+
+    blank_cnt, blank_lst = get_cntlst(game_board)
+    block_cnt, block_lst = get_cntlst(table)
+
+
     def rotate(lst):
-        miny, minx, maxy, maxx = 0, 0, 0, 0
-
+        cnt = len(lst)
+        min_y, min_x = N, N
+        max_y, max_x = 0, 0
         for y, x in lst:
-            miny = min(y, miny)
-            minx = min(x, minx)
-            maxy = max(y, maxy)
-            maxx = max(x, maxx)
+            min_y = min(min_y, y)
+            min_x = min(min_x, x)
+            max_x = max(max_x, x)
+            max_y = max(max_y, y)
 
-        return
+        max_y -= min_y
+        max_x -= min_x
+        temp = [[0]*(max_x+1) for _ in range(max_y+1)]
+        for y, x in lst:
+            temp[y-min_y][x-min_x] = 1
+        
+        rotate_lst = []
+        for i in range(4):
+            if i%2:
+                rotate_lst.append([[0]*(max_y+1) for _ in range(max_x+1)])
+            else:
+                rotate_lst.append([[0]*(max_x+1) for _ in range(max_y+1)])
 
+        for i in range(max_y+1):
+            for j in range(max_x+1):
+                rotate_lst[0][i][j] = temp[i][j]
+                rotate_lst[1][j][-(i+1)] = temp[i][j]
+                rotate_lst[2][-(i+1)][-(j+1)] = temp[i][j]
+                rotate_lst[3][-(j+1)][i] = temp[i][j]
 
-    for i in range(len(blank_lst)):
-        n = len(lst)
-        if n in blank_dic.keys():
-            blank_dic[n].append(rotate(lst))
-        else:
-            blank_dic[n] = [rotate(lst)]
-        blank_lst[i] = [n, y, x, lst]
+        return[cnt, rotate_lst]
+    
 
-    for lst in block_lst:
-        n = len(lst)
-        if n in block_dic.keys():
-            block_dic[n].append(rotate(lst))
-        else:
-            block_dic[n] = [rotate(lst)]
+    for i in range(block_cnt):
+        block_lst[i] = rotate(block_lst[i])
+    for i in range(blank_cnt):
+        blank_lst[i] = rotate(blank_lst[i])
+    
+
+    def is_match(lst1, lst2):
+        y1 = len(lst1[0])
+        x1 = len(lst1[0][0])
+
+        for c in range(len(lst2)):
+            y2 = len(lst2[c])
+            x2 = len(lst2[c][0])
+
+            if y1 != y2 or x1 != x2: continue
+
+            fail = False
+            for i in range(y1):
+                if fail: break
+                for j in range(x1):
+                    if fail: break
+                    if lst1[0][i][j] != lst2[c][i][j]:
+                        fail = True
+            
+            if not fail: return True
+
+        return False
+
 
     answer = 0
+    for i in range(blank_cnt):
+        for j in range(block_cnt):
+            if not block_lst[j][0]: continue
+            if blank_lst[i][0] != block_lst[j][0]: continue
+            if is_match(blank_lst[i][1], block_lst[j][1]):
+                answer += blank_lst[i][0]
+                blank_lst[i][0] = 0
+                block_lst[j][0] = 0
+                break
 
-
-    return
+    return answer
 
 
 for t in range(TC):
