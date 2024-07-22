43681d558aec4f3cd8e7f8b45be338ec39ee0d69
송상빈 <ssbin407@naver.com>
2024-07-22 17:28:44 +0900

    fix baekjoon 16398_행성_연결.py

--- BAEKJOON/16398_행성_연결.py
+++ BAEKJOON/16398_행성_연결.py
@@ -1,5 +1,38 @@
 import sys
 input = sys.stdin.readline
-# sys.setrecursionlimit(1000) # 재귀시 사용
+sys.setrecursionlimit(1500)
 
-'''밑으로 코드'''
+
+def gs(n):
+    while group[n] != n:
+        n = group[n]
+    return n
+
+
+def find():
+    answer = 0
+
+    lst = []
+    for i in range(N):
+        for j in range(i+1, N):
+            lst.append([cost[i][j], i, j])
+    
+    lst.sort()
+
+    for c, i, j in lst:
+        gi = gs(i)
+        gj = gs(j)
+        if gi == gj: continue
+        answer += c
+        if gi < gj:
+            group[gj] = gi
+        else:
+            group[gi] = gj
+
+    return answer
+    
+
+N = int(input())
+cost = [list(map(int, input().split())) for _ in range(N)]
+group = list(range(N))
+print(find())
