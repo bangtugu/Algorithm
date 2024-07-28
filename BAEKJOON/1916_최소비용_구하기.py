a0bc3757e3d2596187a3fbfbaa1628324a907d09
bangtugu <ssbin407@naver.com>
2024-07-28 21:05:57 +0900

    240728 baekjoon 1916_최소비용_구하기.py

---
+++ BAEKJOON/1916_최소비용_구하기.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+M = int(input())
+
+for _ in range(M):
+    a, b, c = map(int, input().split())
+
+a, b = map(int, input().split())
+
+
+
+
+'''
+
+5
+8
+1 2 2
+1 3 3
+1 4 1
+1 5 10
+2 4 2
+3 4 1
+3 5 1
+4 5 3
+1 5
+
+'''
