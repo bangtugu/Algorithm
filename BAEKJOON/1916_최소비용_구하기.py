c9461c80d19857b1ff88f1f66581bd7d055537df
bangtugu <ssbin407@naver.com>
2024-07-29 19:42:22 +0900

    fix baekjoon 1916_최소비용_구하기.py

--- BAEKJOON/1916_최소비용_구하기.py
+++ BAEKJOON/1916_최소비용_구하기.py
@@ -3,13 +3,47 @@ input = sys.stdin.readline
 
 N = int(input())
 M = int(input())
-
+cost = [[-1]*N for _ in range(N)]
+connected = [[] for _ in range(N)]
 for _ in range(M):
     a, b, c = map(int, input().split())
+    a -= 1
+    b -= 1
+    if cost[a][b] == -1:
+        connected[a].append(b)
+        cost[a][b] = c
+    else:
+        cost[a][b] = min(cost[a][b], c)
+
+for i in range(N):
+    connected[i].sort(key = lambda x: cost[i][x])
 
 a, b = map(int, input().split())
+a -= 1
+b -= 1
 
+lst = connected[a][:]
+change = True
+while change:
+    change = False
+    temp = []
+    for m in lst:
+        for j in connected[m]:
+            now = cost[a][m] + cost[m][j]
+            if cost[a][j] == -1:
+                cost[a][j] = now
+                temp.append(j)
+                change = True
+            elif cost[a][b] != -1 and now >= cost[a][b]:
+                continue
+            elif cost[a][j] > now:
+                cost[a][j] = now
+                temp.append(j)
+                change = True
 
+    lst = temp[:]
+
+print(cost[a][b])
 
 
 '''
