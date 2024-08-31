f8721c3dc6b6ca95dc1a281e60429cf916bdd416
bangtugu <ssbin407@naver.com>
2024-08-31 20:39:52 +0900

    fix baekjoon 2981_검문.py

--- BAEKJOON/2981_검문.py
+++ BAEKJOON/2981_검문.py
@@ -1,3 +1,34 @@
 import sys
 input = sys.stdin.readline
 
+N = int(input())
+lst = [int(input()) for _ in range(N)]
+lst.sort()
+
+nlst = [lst[i+1]-lst[i] for i in range(N-1)]
+for i in range(N-1):
+    temp = set()
+    for n in range(2, int(nlst[i]**(1/2))+1):
+        if not nlst[i]%n:
+            temp.add(n)
+            temp.add(nlst[i]//n)
+    temp.add(nlst[i])
+    nlst[i] = temp
+
+answer = nlst[0]
+for i in range(1, N-1):
+    answer = answer & nlst[i]
+
+print(*sorted(list(answer)))
+
+
+'''
+
+A = M * a + R
+B = M * b + R
+C = M * c + R
+
+B - A = M(b - a)
+C - B = M(c - b)
+
+'''
