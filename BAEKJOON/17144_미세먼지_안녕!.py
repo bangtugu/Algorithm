955d94c668f93dd0820705cb847f785de527ad26
bangtugu <ssbin407@naver.com>
2024-09-03 16:17:22 +0900

    fix baekjoon 17144_미세먼지_안녕!.py

--- BAEKJOON/17144_미세먼지_안녕!.py
+++ BAEKJOON/17144_미세먼지_안녕!.py
@@ -1,19 +1,17 @@
-from collections import deque
-
 dy = [-1, 0, 1, 0]
 dx = [0, 1, 0, -1]
 
 
 def step1():
-
+    lst = []
     for i in range(R):
         for j in range(C):
             if room[i][j] > 4:
-                Q.append([i, j, room[i][j]//5])
+                lst.append([i, j, room[i][j]//5])
 
-    while Q:
+    while lst:
 
-        y, x, temp = Q.popleft()
+        y, x, temp = lst.pop()
 
         for i in range(4):
 
@@ -26,74 +24,61 @@ def step1():
 
 
 def step2_up(y):
-
     d = 0
-
-    ny = y + dy[d]
-    nx = 0
-
+    x = 0
     while True:
+        ny = y + dy[d]
+        nx = x + dx[d]
 
-        if 0 > nx + dx[d] or nx + dx[d] >= C or y > ny + dy[d] or ny + dy[d] >= R:
-            d = (d + 3) % 4
+        if ny < 0 or nx < 0 or ny > my or nx >= C:
+            d = (d + 1) % 4
+            continue
+        
+        if room[ny][nx] == -1:
+            room[y][x] = 0
+            break
+        elif room[y][x] != -1:
+            room[y][x] = room[ny][nx]
 
-        nny = ny + dy[d]
-        nnx = nx + dx[d]
-
-        if room[nny][nnx] == -1:
-            room[ny][nx] = 0
-            return
-
-        room[ny][nx] = room[nny][nnx]
-
-        ny = nny
-        nx = nnx
+        y = ny
+        x = nx
 
 
 def step2_down(y):
 
     d = 2
-
-    ny = y + dy[d]
-    nx = 0
+    x = 0
 
     while True:
 
-        if 0 > nx + dx[d] or nx + dx[d] >= C or 0 > ny + dy[d] or ny + dy[d] >= y + 1:
-            d = (d + 1) % 4
+        ny = y + dy[d]
+        nx = x + dx[d]
 
-        nny = ny + dy[d]
-        nnx = nx + dx[d]
+        if ny < my+1 or nx < 0 or ny >= R or nx >= C:
+            d = (d + 3) % 4
+            continue
 
-        if room[nny][nnx] == -1:
-            room[ny][nx] = 0
-            return
+        if room[ny][nx] == -1:
+            room[y][x] = 0
+            break
+        elif room[y][x] != -1:
+            room[y][x] = room[ny][nx]
 
-        room[ny][nx] = room[nny][nnx]
-
-        ny = nny
-        nx = nnx
+        y = ny
+        x = nx
 
 
 R, C, T = map(int, input().split())
-
 room = [list(map(int, input().split())) for _ in range(R)]
-
 my = 0
-
 for i in range(R):
     if room[i][0] == -1:
         my = i
         break
 
-Q = deque()
 for _ in range(T):
     step1()
     step2_up(my)
     step2_down(my+1)
 
-ans = 0
-for line in room:
-    ans += sum(line)
-
-print(ans+2)
+print(sum([sum(line) for line in room])+2)
