e9b0b74143dca9837421f8773ada3ba4239c52a4
bangtugu <ssbin407@naver.com>
2024-09-08 17:26:01 +0900

    fix baekjoon 1509_팰린드롬_분할.py

--- BAEKJOON/1509_팰린드롬_분할.py
+++ BAEKJOON/1509_팰린드롬_분할.py
@@ -1,3 +1,37 @@
 import sys
 input = sys.stdin.readline
+from collections import deque
 
+string = input().split()[0]
+
+lst = [[] for _ in range(len(string))]
+
+for i in range(len(string)):
+    if not string[i]: break
+    str1 = ''
+    str2 = ''
+    for j in range(i, len(string)):
+        str1 = str1 + string[j]
+        str2 = string[j] + str2
+
+        if str1 == str2:
+            lst[i].append(j)
+
+dp = [len(string)]*len(string)
+Q = deque()
+for i in lst[0]:
+    dp[i] = 1
+    Q.append([i+1, 1])
+
+while Q:
+    now, cnt = Q.popleft()
+    if now == len(string):
+        dp[-1] = cnt
+        break
+    
+    for i in lst[now]:
+        if cnt + 1 < dp[i]:
+            Q.append([i+1, cnt+1])
+            dp[i] = cnt+1
+
+print(dp[-1])
