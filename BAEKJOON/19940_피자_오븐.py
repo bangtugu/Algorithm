66ccf5a2ac39916a4f37feed8110bd3f6cbae21d
bangtugu <ssbin407@naver.com>
2024-09-08 17:26:17 +0900

    fix baekjoon 19940_피자_오븐.py

--- BAEKJOON/19940_피자_오븐.py
+++ BAEKJOON/19940_피자_오븐.py
@@ -6,13 +6,31 @@ for _ in range(T):
     N = int(input())
 
     answer = [0, 0, 0, 0, 0]
+
+    answer[0] += N//60
+    N %= 60
+
     if N % 10 < 5:
-        answer[4] += N % 10
+        answer[3] += N % 10
         N -= N % 10
     elif N % 10 > 5:
-        answer[3] += 10 - (N % 10)
+        answer[4] += 10 - (N % 10)
         N += 10 - (N % 10)
-    
-    
+    else:
+        if N + 5 > 40:
+            answer[4] += 5
+            N += 5
+        else:
+            answer[3] += 5
+            N -= 5
 
+    
+    if N % 60 <= 30:
+        answer[1] += (N % 60)//10
+        N -= N % 60
+    else:
+        answer[2] += 6 - (N % 60)//10
+        N += 60 - (N % 60)
+    
+    answer[0] += N//60
     print(*answer)
