2effe9ed622b167ac600604e13f99a42478d140f
bangtugu <ssbin407@naver.com>
2024-10-05 20:13:25 +0900

    fix baekjoon 1647_도시_분할_계획.py

--- BAEKJOON/1647_도시_분할_계획.py
+++ BAEKJOON/1647_도시_분할_계획.py
@@ -1,5 +1,32 @@
 import sys
 input = sys.stdin.readline
-# sys.setrecursionlimit(1000) # 재귀시 사용
 
-'''밑으로 코드'''
+N, M = map(int, input().split())
+G = [i for i in range(N+1)]
+lst = [list(map(int, input().split())) for _ in range(M)]
+lst.sort(key = lambda x: x[2])
+
+
+def leader(n):
+    if G[n] != n:
+        G[n] = leader(G[n])
+    
+    return G[n]
+
+
+cnt = N-2
+answer = 0
+for a, b, c in lst:
+    if not cnt: break
+    A = leader(a)
+    B = leader(b)
+    if A == B: continue
+
+    answer += c
+    cnt -= 1
+    if A < B:
+        G[B] = A
+    else:
+        G[A] = B
+
+print(answer)
