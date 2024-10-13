50c5f9d742480b310d630aef4cef582cc5591919
bangtugu <ssbin407@naver.com>
2024-10-13 23:27:01 +0900

    241013 baekjoon 13334_철로.py

---
+++ BAEKJOON/13334_철로.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+lst = [list(map(int, input().split())) for _ in range(N)]
+d = int(input())
+
+
+
+'''
+8
+5 40
+35 25
+10 20
+10 25
+30 50
+50 60
+30 25
+80 100
+30
+'''
