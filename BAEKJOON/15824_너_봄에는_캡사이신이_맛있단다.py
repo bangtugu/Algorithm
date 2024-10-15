07efa1f90945a5f5873bf746ce3c23e889e44cac
bangtugu <ssbin407@naver.com>
2024-10-16 00:11:05 +0900

    fix baekjoon 15824_너_봄에는_캡사이신이_맛있단다.py

--- BAEKJOON/15824_너_봄에는_캡사이신이_맛있단다.py
+++ BAEKJOON/15824_너_봄에는_캡사이신이_맛있단다.py
@@ -5,4 +5,14 @@ mod = 1000000007
 N = int(input())
 lst = list(map(int, input().split()))
 lst.sort()
+temp = [1]*N
+for i in range(1, N):
+    temp[i] = (temp[i-1]*2)%mod
+answer = 0
+for i in range(N):
+    now = lst[i]
 
+    answer += now*temp[i] + (-now)*temp[N-1-i]
+    answer %= mod
+
+print(answer%mod)
