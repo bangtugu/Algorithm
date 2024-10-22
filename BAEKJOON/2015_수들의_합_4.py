21faa57632179fb39761e104cfda875a7107a6e8
bangtugu <ssbin407@naver.com>
2024-10-22 17:01:38 +0900

    fix baekjoonw 2015_수들의_합_4.py

--- BAEKJOON/2015_수들의_합_4.py
+++ BAEKJOON/2015_수들의_합_4.py
@@ -1,5 +1,26 @@
 import sys
 input = sys.stdin.readline
-# sys.setrecursionlimit(1000) # 재귀시 사용
 
-'''밑으로 코드'''
+N, K = map(int, input().split())
+lst = list(map(int, input().split()))
+
+temp = 0
+sum_dic = {0: 1}
+
+answer = 0
+for i in range(N):
+    temp += lst[i]
+
+    if temp-K in sum_dic:
+        answer += sum_dic[temp-K]
+    
+    if temp not in sum_dic: sum_dic[temp] = 0
+    sum_dic[temp] += 1
+
+print(answer)
+
+
+'''
+4 0
+2 -2 2 -2
+'''
