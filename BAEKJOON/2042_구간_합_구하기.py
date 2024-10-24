50a88ab422c04a2c69b703f4b8aeaa6896334de0
bangtugu <ssbin407@naver.com>
2024-10-24 21:29:57 +0900

    fix baekjoon 2042_구간_합_구하기.py

--- BAEKJOON/2042_구간_합_구하기.py
+++ BAEKJOON/2042_구간_합_구하기.py
@@ -2,8 +2,67 @@ import sys
 input = sys.stdin.readline
 
 N, M, K = map(int, input().split())
+lst = [0] + [int(input()) for _ in range(N)]
+tree = [0]*(N*5)
 
-for _ in range(N):
-    int(input())
 
-for _ in range(M):
+def init_tree(l, r, n):
+
+    if l >= r:
+        tree[n] = lst[l]
+        return tree[n]
+
+    m = (l+r)//2
+    left = init_tree(l, m, n*2)
+    right = init_tree(m+1, r, n*2+1)
+
+    tree[n] = left + right
+
+    return tree[n]
+
+
+def set_tree(l, r, n):
+    if l >= r:
+        tree[n] = lst[b]
+        return lst[b]
+    
+    left = tree[n*2]
+    right = tree[n*2+1]
+
+    m = (l+r)//2
+    if b <= m:
+        left = set_tree(l, m, n*2)
+    else:
+        right = set_tree(m+1, r, n*2+1)
+
+    tree[n] = left + right
+
+    return tree[n]
+
+
+def get_value(l, r, n):
+    
+    if l >= b and r <= c:
+        return tree[n]
+    if c < l or b > r:
+        return 1
+    
+    value = 0
+    m = (l+r)//2
+    if b <= m:
+        value += get_value(l, m, n*2)
+    if c > m:
+        value += get_value(m+1, r, n*2+1)
+
+    return value
+
+
+init_tree(1, N, 1)
+for i in range(M+K):
+    a, b, c = map(int, input().split())
+
+    if a == 1:
+        lst[b] = c
+        set_tree(1, N, 1)
+    if a == 2:
+        print(get_value(1, N, 1))
