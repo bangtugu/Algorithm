050ac591260faf3881dbf739353c01ddbbf0a175
bangtugu <ssbin407@naver.com>
2024-11-05 23:47:41 +0900

    fix baekjoon 22352_항체_인식.py

--- BAEKJOON/22352_항체_인식.py
+++ BAEKJOON/22352_항체_인식.py
@@ -1,5 +1,50 @@
 import sys
 input = sys.stdin.readline
-# sys.setrecursionlimit(1000) # 재귀시 사용
+from collections import deque
 
-'''밑으로 코드'''
+N, M = map(int, input().split())
+before = [list(map(int, input().split())) for _ in range(N)]
+after = [list(map(int, input().split())) for _ in range(N)]
+check = [[0]*M for _ in range(N)]
+dy = [1, -1, 0, 0]
+dx = [0, 0, 1, -1]
+
+
+def bfs(i, j, n):
+    Q = deque()
+    Q.append([i, j])
+    check[i][j] = n
+    target = before[i][j]
+    temp = after[i][j]
+    while Q:
+        y, x = Q.popleft()
+        if temp != after[y][x]:
+            return 2
+
+        for d in range(4):
+            ny = y + dy[d]
+            nx = x + dx[d]
+
+            if ny < 0 or nx < 0 or ny >= N or nx >= M or before[ny][nx] != target or check[ny][nx]: continue
+            check[ny][nx] = n
+            Q.append([ny, nx])
+
+    if before[i][j] != after[i][j]: return 1
+    else: return 0
+
+
+def answer():
+    cnt = 0
+    temp = 0
+    for i in range(N):
+        for j in range(M):
+            if not check[i][j]:
+                cnt += 1
+                temp += bfs(i, j, cnt)
+            if temp >= 2:
+                return 'NO'
+
+    return 'YES'
+
+
+print(answer())
