736ac23e3a45d2c870bf9b08270d3a489236db7f
bangtugu <ssbin407@naver.com>
2024-12-15 23:55:04 +0900

    updata baekjoon

--- BAEKJOON/13702_이상한_술집.py
+++ BAEKJOON/13702_이상한_술집.py
@@ -4,13 +4,11 @@ input = sys.stdin.readline
 N, K = map(int, input().split())
 lst = [int(input()) for _ in range(N)]
 
-s, e = 0, max(lst)
+s, e = 1, max(lst)
 answer = -1
 while s <= e:
 
     m = (s+e)//2
-    if not m:
-        break
     cnt = 0
     for i in range(N):
         cnt += lst[i]//m
