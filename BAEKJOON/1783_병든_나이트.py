27a484bd9b4eebcac7c936964448cb49cedf59a3
bangtugu <ssbin407@naver.com>
2025-02-04 21:33:07 +0900

    250204 baekjoon 1783_병든_나이트.py

--- BAEKJOON/1783_병든_나이트.py
+++ BAEKJOON/1783_병든_나이트.py
@@ -1,5 +1,14 @@
 import sys
 input = sys.stdin.readline
-# sys.setrecursionlimit(1000) # 재귀시 사용
 
-'''밑으로 코드'''
+N, M = map(int, input().split())
+if N == 1:
+    answer = 1
+elif N == 2:
+    answer = min(4, (M-1)//2 + 1)
+elif M < 7:
+    answer = min(4, M)
+else:
+    answer = (2 + (M-5)) + 1
+
+print(answer)
