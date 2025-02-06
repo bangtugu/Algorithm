3203ab738db73d75020d2f434cd9d2974c780b13
bangtugu <ssbin407@naver.com>
2025-02-06 23:54:10 +0900

    250206 baekjoon 11052_카드_구매하기.py

--- BAEKJOON/16500_문자열_판별.py
+++ BAEKJOON/16500_문자열_판별.py
@@ -1,5 +1,25 @@
-import sys
-input = sys.stdin.readline
-# sys.setrecursionlimit(1000) # 재귀시 사용
+string = input()
+N = int(input())
+strdic = {}
+cnt = {}
+for _ in range(N):
+    s = input()
+    if s[0] not in strdic: strdic[s[0]] = []
+    if s not in cnt: cnt[s] = 0
+    strdic[s[0]].append(s)
+    cnt[s] += 1
+check = set()
 
-'''밑으로 코드'''
+
+def checking(i):
+    if i == len(string): return True
+    if string[i] not in strdic: return False
+    for s in strdic[string[i]]:
+        if not cnt[s]: continue
+        if string[i:i+len(s)] == s:
+            cnt[s] -= 1
+            if checking(i+len(s)): return True
+            cnt[s] += 1
+
+
+print(1 if checking(0) else 0)
--- BAEKJOON/9661_돌_게임_7.py
+++ BAEKJOON/9661_돌_게임_7.py
@@ -1,3 +1,6 @@
 import sys
 input = sys.stdin.readline
 
+N = int(input())
+
+print('CY' if N%5 == 2 or not N%5 else 'SK')
