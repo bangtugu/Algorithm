8a7e01b2302326a0c4302da6c854d39096a4e525
bangtugu <ssbin407@naver.com>
2025-02-15 01:11:04 +0900

    250215 baekjoon 2211_네트워크_복구.py

--- BAEKJOON/2211_네트워크_복구.py
+++ BAEKJOON/2211_네트워크_복구.py
@@ -2,7 +2,32 @@ import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
+lines = [[] for _ in range(N+1)]
 
 for _ in range(M):
     A, B, C = map(int, input().split())
-    
+    lines[A].append([B, C])
+    lines[B].append([A, C])
+
+costs = [[-1, -1] for _ in range(N+1)]
+costs[1] = [0, 0]
+
+from heapq import heappush, heappop
+HQ = [[0, 1]]
+
+while HQ:
+    v, now = heappop(HQ)
+    if v > costs[now][0]: continue
+
+    for next, c in lines[now]:
+        if costs[next][0] == -1 or costs[next][0] > v+c:
+            costs[next] = [v+c, now]
+            heappush(HQ, [v+c, next])
+
+connected = []
+for i in range(2, N+1):
+    connected.append([i, costs[i][1]])
+
+print(N-1)
+for couple in connected:
+    print(*couple)
