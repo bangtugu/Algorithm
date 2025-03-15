adfe3dd12f29a94ee95186ab48b3dccce0ebacbf
bangtugu <ssbin407@naver.com>
2025-03-15 23:57:12 +0900

    250315 baekjoon 19236_청소년_상어.py

--- BAEKJOON/19236_청소년_상어.py
+++ BAEKJOON/19236_청소년_상어.py
@@ -1,63 +1,49 @@
 import sys
 input = sys.stdin.readline
-# sys.setrecursionlimit(1000) # 재귀시 사용
 
-di = [-4, -5, -1, 3, 4, 5, 1, -3]
-pool = [[0, 0] for _ in range(16)]
-spot = [-1]*17
-d_lst = [-1]*16
+dy, dx = [-1, -1, 0, 1, 1, 1, 0, -1], [0, -1, -1, -1, 0, 1, 1, 1]
+pool = [[[0, 0] for _ in range(4)] for _ in range(4)]
 
 for i in range(4):
     line = list(map(int, input().split()))
     for j in range(4):
-        n, d = line[j*2], line[j*2+1]-1
-        pool[i*4+j] = n
-        d_lst[n] = d
-        spot[n] = i*4+j
+        pool[i][j] = [line[j*2], line[j*2+1]-1]
+from copy import deepcopy
 
-answer = pool[0]
-spot[pool[0]] = -1
-pool[0] = 0
 
-from collections import deque
-Q = deque()
-Q.append([0, answer, pool, spot, d_lst])
-answer = 0
+def dfs(value, sy, sx, pool):
+    global answer
+    value += pool[sy][sx][0]
+    pool[sy][sx][0] = 0
+    answer = max(answer, value)
 
-while Q:
-    value, pool, spot, d_lst = Q.popleft()
-    #물고기이동
-    for fish in range(1, 17):
-        if spot[fish] == -1: continue
-        idx = spot[fish]
-        d = d_lst[idx]
-        cnt = 0
-        while idx+di[d] < 0 or idx+di[d] >= 16 or pool[idx+di[d]] == 0:
-            d = (d+1)%8
-            cnt += 1
-            if cnt >= 8: break
-        if cnt >= 8: continue
-        d_lst[idx] = d
-        target_idx = idx+di[d]
-        target_fish = pool[target_idx]
-        spot[fish], spot[target_fish] = spot[target_fish], spot[fish]
-        pool[idx], pool[target_idx] = pool[target_idx], pool[idx]
-        d_lst[idx], d_lst[target_idx] = d_lst[target_idx], d_lst[idx]
+    for f in range(1, 17):
+        fy, fx, fd = -1, -1, -1
+        for i in range(4):
+            for j in range(4):
+                if pool[i][j][0] == f:
+                    fy, fx, fd = i, j, pool[i][j][1]
+                    
+        if fy == -1: continue
+        for d in range(8):
+            td = (fd+d)%8
+            ty = fy+dy[td]
+            tx = fx+dx[td]
 
-    #상어이동
-    shark = 0
-    idx = spot[0]
-    d = d_lst[idx]
-    target_idx = idx+di[d]
-    while 0 <= target_idx < 16:
-        if 0 < pool[idx+di[d]] < 17:
-            target_fish = pool[target_idx]
-            spot[0] = spot[target_fish]
-            spot[target_fish] = -1
-            pool[spot[0]] = 0
-            Q.append(value+target_fish, pool, spot, d_lst)
-            pool[spot[0]] = 1
-            j = j+di[d]
+            if max(ty, tx) >= 4 or min(ty, tx) < 0 or (ty == sy and tx == sx): continue
+            pool[fy][fx][1] = td
+            pool[fy][fx], pool[ty][tx] = pool[ty][tx], pool[fy][fx]
+            break
+
+    sd = pool[sy][sx][1]
+    for i in range(1, 4):
+        ny = sy+dy[sd]*i
+        nx = sx+dx[sd]*i
+        if min(ny, nx) < 0 or max(ny, nx) >= 4: break
+        if not pool[ny][nx][0]: continue
+        dfs(value, ny, nx, deepcopy(pool))
+
         
-        target_fish += di[d]
+answer = 0
+dfs(0, 0, 0, deepcopy(pool))
 print(answer)
